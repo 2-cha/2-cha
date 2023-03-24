247c322d059f80a3b34f08bbbbdfb477adc798a7
m1n-q <m1nq.dev@gmail.com>
2023-03-24 20:03:47 +0900

    chore: add h2 database

--- backend/demo/src/main/java/com/_2cha/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/_2cha/demo/DemoApplication.java
@@ -2,24 +2,11 @@ package com._2cha.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-}
-
-@RestController
-class HelloController {
-
-	@RequestMapping("/")
-	public String hello() {
-		return "hello";
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
