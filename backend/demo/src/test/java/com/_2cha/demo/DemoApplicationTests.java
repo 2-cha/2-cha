674d03e4043bbd8b2b2d36054bb5f4ef482d87a6
m1n-q <m1nq.dev@gmail.com>
2023-04-08 01:36:39 +0900

    feat: add QueryDSL

--- backend/demo/src/test/java/com/_2cha/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/_2cha/demo/DemoApplicationTests.java
@@ -1,19 +1,12 @@
 package com._2cha.demo;
 
 
-import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-  @Autowired
-  Faker faker;
-
   @Test
-  void contextLoads() {
-    System.out.println(faker.food().dish());
-  }
+  void contextLoads() {}
 }
