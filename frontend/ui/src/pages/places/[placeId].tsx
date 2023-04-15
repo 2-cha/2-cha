cc5ce5f0c79c9785efae806bf5800e7028cdf578
seushin <seushin@student.42seoul.kr>
2023-04-15 22:39:29 +0900

    render client-side instead of SSR
    edge 환경에서 더더욱 cf에서 SSR은 너무 어렵다
    아래의 상황에서 /places 페이지로 접근해도 정적 파일이 서빙되지 않고
    워커가 응답을 보내는 문제를 해결하지 못했다. 앞으로도 어떤 문제들이
    발생할 지 모르기 때문에 SSR 적용을 보류한다.
    
    - places/
      |- index.tsx
      |- [placeId].tsx (worker)

--- frontend/ui/src/pages/places/[placeId].tsx
+++ frontend/ui/src/pages/places/[placeId].tsx
@@ -1,58 +1,24 @@
-import { GetServerSidePropsContext } from 'next';
-import { Place, QueryResponse } from '@/types';
-import { fetchServer } from '@/lib/fetchServer';
 import NavStackHeader from '@/components/Layout/NavStackHeader';
 import MetaData from '@/components/MetaData';
 import PlaceInfo from '@/components/PlaceInfo';
 import { useIntersection } from '@/hooks/intersection';
+import { usePlaceQuery } from '@/hooks/place';
+import { useRouter } from 'next/router';
 
-// api route를 제외한 runtime은 아직 experimental-edge로 설정해야 함
-export const config = {
-  runtime: 'experimental-edge',
-};
-
-async function fetchPlace(placeId: string) {
-  const response = await fetchServer<QueryResponse<Place>>(
-    `/places/${placeId}`
-  );
-  if (!response.success) {
-    throw response;
-  }
-  return response.data;
-}
-
-interface PlaceInfoPageProps {
-  placeInfo: Place;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  try {
-    const placeId = context.params!.placeId as string;
-    const placeInfo = await fetchPlace(placeId);
-
-    return {
-      props: { placeInfo },
-    };
-  } catch (e) {
-    console.error(e);
-
-    // 404 페이지 렌더
-    return {
-      notFound: true,
-    };
-  }
-}
-
-export default function PlaceInfoPage({ placeInfo }: PlaceInfoPageProps) {
+export default function PlaceInfoPage() {
+  const { query } = useRouter();
+  const { data: placeInfo, isLoading, isError } = usePlaceQuery(query.placeId);
   const { ref, isIntersecting } = useIntersection({ initialState: true });
 
   return (
     <>
-      <MetaData title={placeInfo.name} />
+      <MetaData title={placeInfo?.name} />
       <NavStackHeader hideTitle={isIntersecting}>
-        {placeInfo.name}
+        {placeInfo?.name}
       </NavStackHeader>
-      <PlaceInfo placeInfo={placeInfo} ref={ref} />
+      {isLoading || isError ? null : (
+        <PlaceInfo placeInfo={placeInfo} ref={ref} />
+      )}
       {/* TODO: 지도, 리뷰 */}
     </>
   );
