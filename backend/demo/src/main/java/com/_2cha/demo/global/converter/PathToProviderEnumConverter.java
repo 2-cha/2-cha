cb382102ac606e43cd5f0a3e8640388a6a85cb10
m1n-q <m1nq.dev@gmail.com>
2023-04-20 00:37:05 +0900

    refactor: delegate handling conversion exception to ApiErrorAdvice

--- backend/demo/src/main/java/com/_2cha/demo/global/converter/PathToProviderEnumConverter.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/converter/PathToProviderEnumConverter.java
@@ -1,18 +1,12 @@
 package com._2cha.demo.global.converter;
 
 import com._2cha.demo.member.domain.OIDCProvider;
-import lombok.SneakyThrows;
 import org.springframework.core.convert.converter.Converter;
 
 public class PathToProviderEnumConverter implements Converter<String, OIDCProvider> {
 
-  @SneakyThrows
   @Override
   public OIDCProvider convert(String source) {
-    try {
-      return OIDCProvider.valueOf(source.toUpperCase());
-    } catch (IllegalArgumentException e) {
-      return null;
-    }
+    return OIDCProvider.valueOf(source.toUpperCase());
   }
 }
