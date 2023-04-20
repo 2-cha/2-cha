1df4c25c09744016e3f5bd75699518c2b55a692e
m1n-q <m1nq.dev@gmail.com>
2023-04-20 16:01:36 +0900

    chore: apply configuration profile

--- backend/demo/src/test/java/com/_2cha/demo/member/domain/MemberTests.java
+++ backend/demo/src/test/java/com/_2cha/demo/member/domain/MemberTests.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Transactional
 @SpringBootTest(classes = MockApplication.class)
+@ActiveProfiles({"test"})
 public class MemberTests {
 
   @Autowired
