2373d14ab3baf2e5fbda3890a1da5c9a2f76be1f
m1n-q <68891716+m1n-q@users.noreply.github.com>
2023-05-11 22:29:30 +0900

    feat: refactor place paging (offset based) (#30)
    * apply Pageable
    
    * remove keyset pagination param (min_dist)
    
    * set page param name for backward compatability
    
    * wrap up long params
    
    * remove useless classes
    
    * modify tests
    
    * extract where clause expression as method
    
    * add comments

--- backend/demo/src/main/java/com/_2cha/demo/place/controller/PlaceController.java
+++ backend/demo/src/main/java/com/_2cha/demo/place/controller/PlaceController.java
@@ -2,15 +2,15 @@ package com._2cha.demo.place.controller;
 
 import com._2cha.demo.global.annotation.Auth;
 import com._2cha.demo.place.dto.FilterBy;
-import com._2cha.demo.place.dto.NearbyPlaceRequest;
+import com._2cha.demo.place.dto.NearbyPlaceSearchParams;
 import com._2cha.demo.place.dto.PlaceBriefWithDistanceResponse;
 import com._2cha.demo.place.dto.PlaceDetailResponse;
 import com._2cha.demo.place.dto.SortBy;
 import com._2cha.demo.place.service.PlaceService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +23,6 @@ public class PlaceController {
 
   private final PlaceService placeService;
   private final ObjectMapper objectMapper;
-  private static final String DEFAULT_PAGE_SIZE = "10";
 
   @GetMapping("/places/{placeId}")
   public PlaceDetailResponse getPlaceDetailById(@PathVariable Long placeId) {
@@ -35,24 +34,15 @@ public class PlaceController {
       @RequestParam(name = "filter_by", required = false, defaultValue = "default") FilterBy filterBy,
       @RequestParam(name = "filter_values", required = false) List<String> filterValues,
       @RequestParam(name = "sort_by", required = false, defaultValue = "distance") SortBy sortBy,
-      @RequestParam(name = "page_size", required = false, defaultValue = "10") Integer pageSize,
-      @RequestParam(name = "min_dist") Double minDist,
       @RequestParam(name = "max_dist") Double maxDist,
       @RequestParam(name = "lat") Double lat,
       @RequestParam(name = "lon") Double lon,
-      @RequestParam Map<String, Object> params) {
-
-    params.put("filter_by", filterBy);
-    params.put("sort_by", sortBy);
-    params.put("page_size", pageSize);
-    params.put("filter_values", filterValues);
-    NearbyPlaceRequest dto = objectMapper.convertValue(params, NearbyPlaceRequest.class);
-
-    return placeService.searchPlacesWithFilterAndSorting(dto.getLat(), dto.getLon(),
-                                                         dto.getMinDist(), dto.getMaxDist(),
-                                                         dto.getPageSize(),
-                                                         dto.getSortBy(), dto.getFilterBy(),
-                                                         dto.getFilterValues()
-                                                        );
+      Pageable pageParam) {
+    NearbyPlaceSearchParams searchParams = new NearbyPlaceSearchParams(lat, lon, maxDist, filterBy,
+                                                                       filterValues, sortBy,
+                                                                       pageParam.getOffset(),
+                                                                       pageParam.getPageSize()
+    );
+    return placeService.searchPlacesWithFilterAndSorting(searchParams);
   }
 }
