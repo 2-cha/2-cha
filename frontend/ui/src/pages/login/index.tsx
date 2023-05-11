2c0fbe141b754cae19bda4167fe12f5480ea18aa
chichoon <chichoon.choi@gmail.com>
2023-05-11 13:26:24 +0900

    feat(ui): login

--- frontend/ui/src/pages/login/index.tsx
+++ frontend/ui/src/pages/login/index.tsx
@@ -1,10 +1,5 @@
-import { GOOGLE_AUTH_URL } from '@/lib/auth';
+import Login from '@/components/Login';
 
 export default function LogIn() {
-  return (
-    <div>
-      <p>Sign in</p>
-      <a href={GOOGLE_AUTH_URL}>Continue with Google</a>
-    </div>
-  );
+  return <Login />;
 }
