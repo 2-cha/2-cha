399a478f95b348da81130fe5694476ad2c62f513
m1n-q <m1nq.dev@gmail.com>
2023-05-11 13:06:43 +0900

    (collection) set img url path to VARCHAR

--- backend/demo/src/main/java/com/_2cha/demo/collection/domain/Collection.java
+++ backend/demo/src/main/java/com/_2cha/demo/collection/domain/Collection.java
@@ -9,7 +9,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
@@ -33,7 +32,6 @@ public class Collection {
 
   private boolean isExposed = true;
 
-  @Lob
   @Column(nullable = false)
   private String thumbnailUrlPath;
 
