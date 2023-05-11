dc0a93986c9d1077d36995b6302d783cb7cc1cc3
chichoon <chichoon.choi@gmail.com>
2023-05-11 15:10:59 +0900

    feat: added profile collection area

--- frontend/ui/src/components/Profile/index.tsx
+++ frontend/ui/src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import { Member } from '@/types';
 import ProfileHeader from './ProfileHeader';
+import ProfileCollection from './ProfileCollection';
 
 interface Props {
   member: Member;
@@ -9,7 +10,7 @@ export default function Profile({ member }: Props) {
   return (
     <>
       <ProfileHeader member={member} />
-      <div>컬렉션</div>
+      <ProfileCollection />
     </>
   );
 }
