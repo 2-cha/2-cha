475160c3695ea55ab807bde25ac3762913a7d73e
seushin <74958130+seushin@users.noreply.github.com>
2023-05-12 18:40:41 +0900

    feat(ui): place reviews layout (#32)
    * update mock
    
    * fix PlaceReviews layout
    
    * fix attr name to camel case
    
    * set background color to header

--- frontend/ui/src/components/PlaceInfo/PlaceReviewsMenu/index.tsx
+++ frontend/ui/src/components/PlaceInfo/PlaceReviewsMenu/index.tsx
@@ -1,16 +1,41 @@
 import { usePlaceReviewsQuery } from '@/hooks/query/usePlaceReviews';
 import s from './PlaceReviewsMenu.module.scss';
+import Link from 'next/link';
 
 export default function PlaceReviews({ placeId }: { placeId: number }) {
   const { data: reviews, isLoading, isError } = usePlaceReviewsQuery(placeId);
 
-  // TODO: 레이아웃 디자인, 무한스크롤
+  // TODO: 무한스크롤, 이미지 슬라이드
   return (
     <div className={s.root}>
       {reviews?.length ? (
-        <ul>
+        <ul className={s.review__list}>
           {reviews.map((review) => (
-            <li key={review.id}>{JSON.stringify(review)}</li>
+            <li key={review.id} className={s.review__item}>
+              <div className={s.header}>
+                <div className={s.header__profile}>
+                  <img src={review.member.prof_img} alt={review.member.name} />
+                </div>
+                <Link href={`/member/${review.member.id}`}>
+                  <p className={s.review__title}>{review.member.name}</p>
+                </Link>
+              </div>
+              <div className={s.review__image}>
+                <img
+                  src={review.images[0]}
+                  alt={`${review.place.name}-${review.id}`}
+                />
+              </div>
+              <div className={s.tags}>
+                {review.tags.map((tag) => (
+                  <div key={tag.id} className={s.tag}>
+                    <span>
+                      {tag.emoji} {tag.message}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </li>
           ))}
         </ul>
       ) : (
--- frontend/ui/src/types/review.ts
+++ frontend/ui/src/types/review.ts
@@ -1,8 +1,9 @@
-import type { Tag, Member } from '.';
+import type { Tag, Member, Place } from '.';
 
 export interface Review {
   id: number;
   member: Member;
+  place: Place;
   images: string[];
   tags: Tag[];
 }
