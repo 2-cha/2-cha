08d07004eb3c2fd4393fba54d5371c3db15ac42c
seushin <seushin@student.42seoul.kr>
2023-05-15 18:31:23 +0900

    refactor `ImagePicker`

--- frontend/ui/src/components/WriteReviewForm/ImagePicker/index.tsx
+++ frontend/ui/src/components/WriteReviewForm/ImagePicker/index.tsx
@@ -1,41 +1,23 @@
-import { useEffect, useRef, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { useReviewImageMutation } from '@/hooks/mutation/useReviewImage';
+import { useImagePicker, type Image } from '@/hooks/useImagePicker';
 import { type ReviewFormData } from '@/components/WriteReviewForm';
 import s from './ImagePicker.module.scss';
 
-function isSameFile(file1: File, file2: File) {
-  return (
-    file1.name === file2.name &&
-    file1.size === file2.size &&
-    file1.lastModified === file2.lastModified
-  );
-}
-
 interface ImagePickerProps {
   name: keyof ReviewFormData;
-}
-
-export interface Image {
-  file: File;
-  url?: string;
-  isError?: boolean;
 }
 
 export default function ImagePicker({ name }: ImagePickerProps) {
   const { register, setValue } = useFormContext<ReviewFormData>();
   register(name, { required: true });
 
-  const [images, setImages] = useState<Image[]>([]);
-  const imageMutation = useReviewImageMutation();
-  const imageRef = useRef<HTMLInputElement | null>(null);
-
-  useEffect(() => {
-    setValue(
-      name,
-      images.map((image) => image.url).filter(Boolean) as string[]
-    );
-  }, [images, name, setValue]);
+  const { ref, images, removeImage, handleChange } = useImagePicker({
+    onChange: (images: Image[]) =>
+      setValue(
+        name,
+        images.map((image) => image.url).filter(Boolean) as string[]
+      ),
+  });
 
   return (
     <div className={s.root}>
@@ -51,11 +33,7 @@ export default function ImagePicker({ name }: ImagePickerProps) {
           <button
             className={s.image__remove}
             type="button"
-            onClick={() => {
-              setImages((prev) =>
-                prev.filter((img) => !isSameFile(img.file, image.file))
-              );
-            }}
+            onClick={() => removeImage(image.file)}
           >
             x
           </button>
@@ -63,56 +41,17 @@ export default function ImagePicker({ name }: ImagePickerProps) {
       ))}
       <div className={s.imageInput}>
         <input
-          ref={imageRef}
+          ref={ref}
           type="file"
           accept="image/*"
           multiple
           hidden
-          onChange={(e) => {
-            const newFiles = e.target.files;
-            if (!newFiles) {
-              return;
-            }
-
-            const newImages = Array.from(newFiles)
-              .filter(
-                (file) => !images.find((image) => isSameFile(image.file, file))
-              )
-              .map((file) => {
-                imageMutation.mutate(file, {
-                  onSuccess: (url) => {
-                    setImages((prev) => {
-                      const target = prev.find((image) =>
-                        isSameFile(image.file, file)
-                      );
-                      if (target) {
-                        target.url = url;
-                      }
-                      return [...prev];
-                    });
-                  },
-                  onError: () => {
-                    setImages((prev) => {
-                      const target = prev.find((image) =>
-                        isSameFile(image.file, file)
-                      );
-                      if (target) {
-                        target.isError = true;
-                      }
-                      return [...prev];
-                    });
-                  },
-                });
-                return { file };
-              });
-            setImages((prev) => [...prev, ...newImages]);
-            e.target.value = '';
-          }}
+          onChange={handleChange}
         />
         <button
           className={s.addButton}
           type="button"
-          onClick={() => imageRef.current?.click()}
+          onClick={() => ref.current?.click()}
         >
           <span>이미지 추가</span>
         </button>
