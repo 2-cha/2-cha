8cdfb6a0cf6f8227b7f33c8b8c116f263ccc37f8
chichoon <chichoon.choi@gmail.com>
2023-05-29 16:35:08 +0900

    feat: Tag list foldable

--- frontend/ui/src/components/Tags/index.tsx
+++ frontend/ui/src/components/Tags/index.tsx
@@ -1,5 +1,7 @@
+import { useCallback, useState } from 'react';
 import cx from 'classnames';
 
+import SimpleArrowIcon from '@/components/Icons/SimpleArrowIcon';
 import { Tag } from '@/types';
 
 import styles from './Tags.module.scss';
@@ -7,17 +9,63 @@ import styles from './Tags.module.scss';
 interface Props {
   tagList: Tag[];
   keyID: string;
+  limit: number;
   className?: string;
-  limit?: number;
+  isNumberShown?: boolean;
 }
 
-export default function Tags({ tagList, keyID, limit, className }: Props) {
-  if (limit) tagList = tagList.slice(0, limit);
+interface TagProperty {
+  hasLimit: boolean;
+  topTags: Tag[];
+  bottomTags: Tag[];
+}
+
+export default function Tags({ tagList, keyID, limit = 0, className }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const tagProperty: TagProperty = {
+    hasLimit: false,
+    topTags: tagList,
+    bottomTags: [],
+  };
+
+  if (limit > 0 && limit < tagList.length) {
+    tagProperty.hasLimit = true;
+    tagProperty.topTags = tagList.slice(0, limit);
+    tagProperty.bottomTags = tagList.slice(limit);
+  }
+
+  const handleClickButton = useCallback(
+    function () {
+      setIsOpen((prevState) => !prevState);
+    },
+    [setIsOpen]
+  );
 
   return (
     <div className={cx(styles.tags, className)}>
-      {tagList.map((tag) => (
-        <div key={`tag-list-${keyID}-${tag.id}`} className={styles.tag}>
+      <button
+        type="button"
+        className={cx({
+          [styles.button]: true,
+          [styles.hidden]: !tagProperty.hasLimit,
+          [styles.flipped]: isOpen,
+        })}
+        onClick={handleClickButton}
+      >
+        <SimpleArrowIcon />
+      </button>
+      {tagProperty.topTags.map((tag, index) => (
+        <div key={`tag-list-${keyID}-${index}`} className={styles.tag}>
+          <span>
+            {tag.emoji} {tag.message}
+          </span>
+        </div>
+      ))}
+      {tagProperty.bottomTags.map((tag, index) => (
+        <div
+          key={`tag-list-${keyID}-${index + limit}`}
+          className={cx({ [styles.tag]: true, [styles.hidden]: !isOpen })}
+        >
           <span>
             {tag.emoji} {tag.message}
           </span>
