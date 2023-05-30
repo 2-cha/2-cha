05b8bf3af2dcb8630b48c12694886c004780bdfa
seushin <seushin2021@gmail.com>
2023-05-30 13:44:33 +0900

    remove temp token

--- frontend/ui/src/lib/fetchClient.ts
+++ frontend/ui/src/lib/fetchClient.ts
@@ -9,8 +9,8 @@ export const fetchClient = axios.create({
 });
 
 fetchClient.interceptors.request.use((config) => {
-  // TODO: 실제 토큰으로 교체
-  const token = process.env.NEXT_PUBLIC_TEMP_AUTH_TOKEN;
+  let token;
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
