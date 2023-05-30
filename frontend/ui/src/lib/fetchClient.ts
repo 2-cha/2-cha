c21873ff764eef82fc515818773a0ca9b2136034
seushin <74958130+seushin@users.noreply.github.com>
2023-05-30 14:19:11 +0900

    fix(ui): remove temp token (#57)
    * remove temp token
    
    * get token from localStorage
    
    ---------
    
    Co-authored-by: seushin <seushin2021@gmail.com>

--- frontend/ui/src/lib/fetchClient.ts
+++ frontend/ui/src/lib/fetchClient.ts
@@ -1,4 +1,21 @@
 import axios from 'axios';
+import { RECOIL_PERSIST_KEY } from '@/atoms/persist';
+import { tokenState, type Token } from '@/atoms/token';
+
+function getToken(): Token | undefined {
+  try {
+    if (typeof window !== 'undefined') {
+      const recoilStorage = window.localStorage.getItem(RECOIL_PERSIST_KEY);
+      if (recoilStorage) {
+        const recoilState = JSON.parse(recoilStorage);
+        const token = recoilState[tokenState.key];
+        return token;
+      }
+    }
+  } catch {}
+
+  return undefined;
+}
 
 export const fetchClient = axios.create({
   baseURL:
@@ -9,10 +26,10 @@ export const fetchClient = axios.create({
 });
 
 fetchClient.interceptors.request.use((config) => {
-  // TODO: 실제 토큰으로 교체
-  const token = process.env.NEXT_PUBLIC_TEMP_AUTH_TOKEN;
+  const token = getToken();
+
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token.access_token}`;
   }
   return config;
 });
