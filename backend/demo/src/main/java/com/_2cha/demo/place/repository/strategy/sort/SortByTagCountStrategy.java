9ce3110d676acfa431da5e42845ffabb1bfe5348
m1n-q <m1nq.dev@gmail.com>
2023-06-04 21:19:39 +0900

    modify `SortByTagCountStrategy` to use only filtered tags

--- backend/demo/src/main/java/com/_2cha/demo/place/repository/strategy/sort/SortByTagCountStrategy.java
+++ backend/demo/src/main/java/com/_2cha/demo/place/repository/strategy/sort/SortByTagCountStrategy.java
@@ -2,8 +2,10 @@ package com._2cha.demo.place.repository.strategy.sort;
 
 import static com._2cha.demo.place.domain.QPlace.place;
 import static com._2cha.demo.review.domain.QReview.review;
+import static com._2cha.demo.review.domain.QTagInReview.tagInReview;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -16,10 +18,15 @@ public class SortByTagCountStrategy implements
   public JPAQuery<Tuple> apply(JPAQueryFactory q,
                                NumberExpression<Double> distanceSphere,
                                List<?> filterValues) {
-
-    return q.select(place, distanceSphere, review.tagsInReview.size().count())
-            .from(place).leftJoin(review).on(review.place.id.eq(place.id))
+    return q.select(place, distanceSphere, tagInReview.tag.count())
+            .from(place)
+            .leftJoin(review).on(review.place.id.eq(place.id))
+            .leftJoin(review.tagsInReview, tagInReview).on(anyTagsIn((List<Long>) filterValues))
             .groupBy(place)
-            .orderBy(review.tagsInReview.size().count().desc(), distanceSphere.asc());
+            .orderBy(tagInReview.tag.count().desc(), distanceSphere.asc());
+  }
+
+  private BooleanExpression anyTagsIn(List<Long> filterValues) {
+    return tagInReview.tag.id.in(filterValues);
   }
 }
