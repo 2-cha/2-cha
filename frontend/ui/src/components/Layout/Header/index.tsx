a4dce18e18dc9b4caaafbfbb1f06426dfe45d13e
seushin <seushin2021@gmail.com>
2023-06-06 15:38:21 +0900

    fix `SearchAddressForm` to modal

--- frontend/ui/src/components/Layout/Header/index.tsx
+++ frontend/ui/src/components/Layout/Header/index.tsx
@@ -1,110 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useCurrentLocation } from '@/hooks/useCurrentLocation';
 import { useEupMyeonDong } from '@/hooks/query/useRegion';
-import { useAddressQuery } from '@/hooks/query/useAddress';
-import { useSetRecoilState } from 'recoil';
-import { locationState } from '@/atoms/location';
-import { type Address } from '@/pages/api/address';
+import { useModal } from '@/hooks/useModal';
 import LocationIcon from '@/components/Icons/LocationIcon';
+import SearchAddressModal from './SearchAddressModal';
 import s from './Header.module.scss';
 
 export default function Header() {
   const { location, isError } = useCurrentLocation();
-  const [isFormOpen, setFormOpen] = useState(false);
+  const { isOpen, onOpen, onClose } = useModal();
 
   useEffect(() => {
     if (!location && isError) {
-      setFormOpen(true);
+      onOpen();
     }
   }, [location, isError]);
 
   // 읍면동
   let eupMyeonDong = useEupMyeonDong(location);
   if (!eupMyeonDong && isError) {
-    eupMyeonDong = '주소를 검색해주세요';
+    eupMyeonDong = '위치를 알 수 없어요';
   }
-
-  // 주소 검색
-  const [query, setQuery] = useState('');
-  const { data: addresses } = useAddressQuery(query);
 
   return (
     <div className={s.root}>
       <header className={s.header}>
-        <button
-          onClick={() => setFormOpen((prev) => !prev)}
-          className={s.header__button}
-        >
+        <button onClick={onOpen} className={s.header__button}>
           <LocationIcon />
-        </button>
-        {isFormOpen ? (
-          <SearchAddressForm onSubmit={(address) => setQuery(address)} />
-        ) : (
           <p className={s.header__title}>{eupMyeonDong}</p>
-        )}
+        </button>
       </header>
-      {addresses && isFormOpen ? (
-        <AddressList
-          addresses={addresses}
-          onSelected={() => setFormOpen(false)}
-        />
-      ) : null}
+      <SearchAddressModal isOpen={isOpen} onClose={onClose} />
     </div>
-  );
-}
-
-interface SearchAddressFormProps {
-  onSubmit: (address: string) => void;
-}
-
-function SearchAddressForm({ onSubmit }: SearchAddressFormProps) {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const address = formData.get('address') as string;
-    onSubmit(address);
-  };
-
-  return (
-    <form className={s.searchForm} onSubmit={handleSubmit}>
-      <input
-        className={s.searchForm__input}
-        type="text"
-        id="address"
-        name="address"
-        placeholder="주소를 입력하세요"
-      />
-      <input className={s.searchForm__button} type="submit" value="검색" />
-    </form>
-  );
-}
-
-interface AddressListProps {
-  addresses: Address[];
-  onSelected?: () => void;
-}
-
-function AddressList({ addresses, onSelected }: AddressListProps) {
-  const setLocationState = useSetRecoilState(locationState);
-
-  return (
-    <ul className={s.addressList}>
-      {addresses.length === 0 && <li>검색 결과가 없습니다.</li>}
-      {addresses.map((address) => (
-        <li key={address.address_name}>
-          <button
-            onClick={() => {
-              onSelected && onSelected();
-              setLocationState({
-                lat: Number(address.y),
-                lon: Number(address.x),
-              });
-            }}
-          >
-            {address.address_name}
-          </button>
-        </li>
-      ))}
-    </ul>
   );
 }
