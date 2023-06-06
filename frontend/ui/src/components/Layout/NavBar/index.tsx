80685a232f5f2a1cd4b0bc592609ed4a629c099c
Ji Yoon Choi <chichoon.choi@gmail.com>
2023-06-06 15:44:28 +0900

    Feat: review icon (#69)
    * feat: 태그 리스트 컴포넌트 분리
    
    * feat: taglist limit
    
    * style: add classname for taglist
    
    * feat: Tag list foldable
    
    * style: arrow icon with no stick
    
    * feat: tag count 출력
    
    * feat: tag list gradient
    
    * style: 화살표 버튼 위치 수정
    
    * feat: add tag limit to reviews
    
    * chore: .
    
    * feat: set default div for no image
    
    * feat: add review button
    
    * feat: :sparkles: 버튼 위치 이동 (로딩중에도 출력되도록)
    
    * chore: review button 삭제
    
    * feat: review add button 하단바로 이동
    
    * style: review icon 그림자로 수정

--- frontend/ui/src/components/Layout/NavBar/index.tsx
+++ frontend/ui/src/components/Layout/NavBar/index.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
+
 import PlaceIcon from '@/components/Icons/PlaceIcon';
 import HashIcon from '@/components/Icons/HashIcon';
 import BookmarkIcon from '@/components/Icons/BookmarkIcon';
 import UserIcon from '@/components/Icons/UserIcon';
-import cn from 'classnames';
+import PlusSquareIcon from '@/components/Icons/PlusSquareIcon';
+
 import s from './NavBar.module.scss';
 
 const sections = [
   { name: 'places', path: '/places', Icon: PlaceIcon },
   { name: 'reviews', path: '/reviews', Icon: HashIcon },
+  { name: 'add-review', path: '/write', Icon: PlusSquareIcon },
   { name: 'bookmark', path: '/bookmark', Icon: BookmarkIcon },
   { name: 'profile', path: '/profile', Icon: UserIcon },
 ];
@@ -20,16 +25,19 @@ interface NavBarProps {
 export default function NavBar({ currentSection }: NavBarProps) {
   return (
     <nav className={s.navbar}>
-      {sections.map((section) => (
-        <NavItem
-          key={section.name}
-          url={section.path}
-          isActive={currentSection === section.name}
-        >
-          <section.Icon isActive={currentSection === section.name} />
-          {section.name}
-        </NavItem>
-      ))}
+      {sections.map((section) =>
+        section.name === 'add-review' ? (
+          <ReviewAddButton key={section.name} />
+        ) : (
+          <NavItem
+            key={section.name}
+            url={section.path}
+            isActive={currentSection === section.name}
+          >
+            <section.Icon isActive={currentSection === section.name} />
+          </NavItem>
+        )
+      )}
     </nav>
   );
 }
@@ -40,11 +48,24 @@ interface NavItemProps {
   children: React.ReactNode;
 }
 
-export function NavItem({ isActive, url, children }: NavItemProps) {
+function NavItem({ isActive, url, children }: NavItemProps) {
   return (
     <div className={cn(s.navbar__item, { [s.navbar__item_active]: isActive })}>
       <Link href={url} className={s.navbar__link}>
         {children}
+      </Link>
+    </div>
+  );
+}
+
+function ReviewAddButton() {
+  const {
+    query: { placeId },
+  } = useRouter();
+  return (
+    <div className={s.addButton}>
+      <Link href={`/write${placeId ? `?placeId=${placeId}` : ''}`}>
+        <PlusSquareIcon width={50} height={50} withoutBorder />
       </Link>
     </div>
   );
