0e55492a305dfb8f1f5f9056e3a63e62f8ee19c5
seushin <seushin@student.42seoul.kr>
2023-06-07 18:16:22 +0900

    use existing icon

--- frontend/ui/src/components/BookmarkToggleButton/index.tsx
+++ frontend/ui/src/components/BookmarkToggleButton/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import cn from 'classnames';
 import s from './BookmarkToggleButton.module.scss';
 import { useBookmakrMutation } from '@/hooks/mutation/useBookmark';
+import BookmarkIcon from '@/components/Icons/BookmarkIcon';
 
 interface BookmarkButtonProps {
   isBookmarked?: boolean;
@@ -42,43 +43,12 @@ export function BookmarkToggleButton({
       onClick={handleClick}
       {...props}
     >
-      {isBookmarked ? (
-        <FilledBookmarkIcon size={size} />
-      ) : (
-        <StrokeBookmarkIcon size={size} />
-      )}
+      <BookmarkIcon
+        width={size}
+        height={size}
+        isSingle
+        isActive={isBookmarked}
+      />
     </button>
-  );
-}
-
-function StrokeBookmarkIcon({ size }: { size: number }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill="currentColor"
-        d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
-      />
-    </svg>
-  );
-}
-
-function FilledBookmarkIcon({ size }: { size: number }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill="currentColor"
-        d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"
-      />
-    </svg>
   );
 }
