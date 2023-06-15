34b4b32f8da7382fd4976ea97a7827f6b1bdb346
m1n-q <m1nq.dev@gmail.com>
2023-06-15 18:40:31 +0900

    add javadoc

--- backend/demo/src/main/java/com/_2cha/demo/global/annotation/Auth.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/annotation/Auth.java
@@ -7,6 +7,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.core.annotation.AliasFor;
 
+/**
+ * If the token exists, it will be validated and `memberId` will be injected to @Authed argument.
+ * <p>
+ * Note that @Auth(GUEST) will pass request even if token is not included.
+ * <p>
+ * So, @Authed argument will be null.
+ * <p>
+ * But if the token exists, the memberId will be provided even if it is a guest privilege handler.
+ * <p>
+ * <p>
+ * <p>
+ * It allows higher privilege. e.g.) @Auth(GUEST) allows MEMBER, ADMIN.
+ * <p>
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
