743d1ad3f458e2b9e1426269a14d3b761b2932f7
m1n-q <m1nq.dev@gmail.com>
2023-06-16 16:53:10 +0900

    fix: tag filter
    this commit fixes #88

--- backend/demo/src/main/java/com/_2cha/demo/place/repository/strategy/filter/TagFilterStrategy.java
+++ backend/demo/src/main/java/com/_2cha/demo/place/repository/strategy/filter/TagFilterStrategy.java
@@ -3,10 +3,9 @@ package com._2cha.demo.place.repository.strategy.filter;
 import static com._2cha.demo.place.domain.QPlace.place;
 import static com._2cha.demo.review.domain.QReview.review;
 import static com._2cha.demo.review.domain.QTagInReview.tagInReview;
+import static java.lang.Long.valueOf;
 
-import com._2cha.demo.place.domain.Place;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -19,17 +18,19 @@ public class TagFilterStrategy implements FilterStrategy {
   @Override
   public JPAQuery<Tuple> apply(JPAQuery<Tuple> q, List<?> filterValues) {
 
-    JPQLQuery<Place> subQuery = JPAExpressions.select(review.place)
-                                              .from(review)
-                                              .join(review.place)
-                                              .join(review.tagsInReview, tagInReview)
-                                              .join(tagInReview.tag)
-                                              .where(anyTagsIn((List<Long>) filterValues));
+    JPQLQuery<Long> subQuery = JPAExpressions.select(review.place.id)
+                                             .from(review)
+                                             .join(tagInReview)
+                                             .on(tagInReview.review.id.eq(review.id))
+                                             .where(
+                                                 tagInReview.tag.id.in((List<Long>) filterValues)
+                                                   )
+                                             .groupBy(review.place.id)
+                                             .having(
+                                                 tagInReview.tag.id.countDistinct()
+                                                                   .eq(valueOf(
+                                                                       filterValues.size())));
 
-    return q.where(place.in(subQuery));
-  }
-
-  private BooleanExpression anyTagsIn(List<Long> filterValues) {
-    return review.tagsInReview.any().tag.id.in(filterValues);
+    return q.where(place.id.in(subQuery));
   }
 }
