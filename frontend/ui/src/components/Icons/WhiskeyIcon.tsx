3761131bbc7363a2fadc47404f62e0e46cfe3c0a
m1n-q <68891716+m1n-q@users.noreply.github.com>
2023-06-18 20:40:11 +0900

    add icons (#87)

---
+++ frontend/ui/src/components/Icons/WhiskeyIcon.tsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import { memo } from 'react';
+
+export default memo(function WhiskeyIcon({
+  width = 48,
+  height = 48,
+  ...props
+}: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <path
+        d="M43.645 79.34v143.365c-.175.325.021.65.011.977 0 6.026 27.254 10.912 61.23 10.912h.313c33.977 0 61.231-4.886 61.23-10.912-.01-.326.187-.652.012-.977V79.34l-38.627.006H82.271l-38.627-.006z"
+        className="UnoptimicedTransforms"
+        style={{
+          fill: '#fff',
+          fillOpacity: 0.996454,
+          stroke: 'none',
+          strokeWidth: 5.91867,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+        transform="matrix(.99943 0 0 1.02457 23.017 -32.633)"
+      />
+      <g
+        style={{
+          fill: '#a0c5d1',
+          fillOpacity: 0.606618,
+        }}
+        transform="matrix(.71285 .18591 -.18636 .7146 223.037 87.78)"
+      >
+        <circle
+          cx={-113.652}
+          cy={87.047}
+          r={59.863}
+          style={{
+            fill: '#7ebbdc',
+            fillOpacity: 0.858156,
+            stroke: '#000',
+            strokeWidth: 6.365,
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <path
+          d="M-103.94 129.37c9.225-.046 18.343-4.37 24.217-11.484a31.739 31.739 0 0 0 6.32-12.573"
+          style={{
+            fill: 'none',
+            fillOpacity: 0.606618,
+            stroke: '#000',
+            strokeWidth: 5.77846,
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <ellipse
+          cx={-60.529}
+          cy={103.993}
+          rx={2.085}
+          ry={4.224}
+          style={{
+            fill: '#000',
+            fillOpacity: 1,
+            stroke: '#000',
+            strokeWidth: 1.28475,
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+          transform="matrix(.9891 .14726 -.10354 .99463 0 0)"
+        />
+      </g>
+      <path
+        d="M116.154 210.424c-22.374-.838-42.356-4.312-47.051-8.181l-.537-.443V49.437h120.422l-.002 75.2c-.001 41.36-.06 75.702-.13 76.315-.112.98-.216 1.178-.85 1.63-1.805 1.288-7.743 3.201-13.605 4.383-14.183 2.859-37.415 4.238-58.248 3.459z"
+        style={{
+          fill: '#3c72d0',
+          fillOpacity: 0.195035,
+          stroke: 'none',
+          strokeWidth: 1.05804,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M121.634 192.626c-23.138-.348-44.492-2.98-52.004-6.413l-1.95-.891v-52.906l-1.562-.04c-7.333-.192 9.503 2.24 26.172 3.466 21.331 1.568 18.99.98 32.045 1.475 9.01.342 12.706.374 28.46.361 15.89-.014 21.155-.965 35.898-3.8 2.876-.554.278.547.4.547.255 0 .3 48.497.045 49.966-.151.877-.328 1.056-1.683 1.71-8.62 4.158-36.88 6.96-65.82 6.525z"
+        style={{
+          fill: '#d47008',
+          fillOpacity: 0.698582,
+          stroke: 'none',
+          strokeWidth: 1.35713,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M102.99 192.262c-20.607.431-27.545-5.548-33.476-8.332l-1.907-.895v-4.213c0-2.317.07-6.107.152-8.42l.151-4.208.603.303c1.1.554-5.388 14.993-2.693 15.657 6.085 1.501 15.192 1.356 30.78 1.711 16.298.371 30.794-1.579 40.398-2.496 4.712-.45 10.451-2.316 19.234-6.426.398-.186 16.792-6.095 16.848-21.423l.059-16.235 3.675-.514c3.178-.445 7.637-1.188 9.33-1.556l.52-.113-.058 24.028-.059 24.029-1.156.612c-3.56 1.886-11.962 6.392-23.245 7.613-12.088 1.309-14.176 2.937-33.721 3.025-10.108.046-19.731-1.584-21.384-1.697z"
+        style={{
+          fill: '#905410',
+          fillOpacity: 0.368794,
+          stroke: 'none',
+          strokeWidth: 1.17676,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M173.08 93.08V48.813h16.362l-.13 43.045c-.07 23.675-.173 43.087-.227 43.138-.156.149-5.07.91-9.973 1.545-2.489.322-4.863.636-5.278.698l-.754.112z"
+        style={{
+          fill: '#6491a3',
+          fillOpacity: 0.503546,
+          stroke: 'none',
+          strokeWidth: 1.30721,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M66.922 49.058v133.42c-.174.302.022.605.011.908 0 5.608 27.108 10.155 60.903 10.155s61.191-4.547 61.19-10.155c0-.23-.048-.462-.143-.692h.156V49.058a3075176.071 3075176.071 0 0 1-61.059.004h-22.638l-38.42-.004z"
+        style={{
+          fill: 'none',
+          fillOpacity: 1,
+          stroke: '#000',
+          strokeWidth: 5.69436,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M43.645 79.34v143.365c-.175.325.021.65.011.977 0 6.026 27.254 10.912 61.23 10.912h.313c33.977 0 61.231-4.886 61.23-10.912-.01-.326.187-.652.012-.977V79.34l-38.627.006H82.271l-38.627-.006z"
+        className="UnoptimicedTransforms"
+        style={{
+          fill: 'none',
+          fillOpacity: 1,
+          stroke: '#000',
+          strokeWidth: 5.91867,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+        transform="matrix(.99943 0 0 1.02457 23.017 -32.896)"
+      />
+    </svg>
+  );
+});
