1e551aa9bc5c6758ac201aee6ff182e133de9441
m1n-q <m1nq.dev@gmail.com>
2023-06-20 16:33:43 +0900

    use name generator when sign up with oidc

--- backend/demo/src/main/java/com/_2cha/demo/member/repository/MemberRepository.java
+++ backend/demo/src/main/java/com/_2cha/demo/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com._2cha.demo.member.repository;
 
 import com._2cha.demo.member.domain.Member;
+import java.util.List;
 import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
@@ -12,4 +13,6 @@ public interface MemberRepository extends Repository<Member, Long> {
   Member findById(Long id);
 
   Member findByEmail(String email);
+
+  List<Member> findAllByNameIn(List<String> names);
 }
