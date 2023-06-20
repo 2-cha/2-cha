37539d763a5bd9f689b3f925783e86986e79b549
m1n-q <m1nq.dev@gmail.com>
2023-06-20 16:32:00 +0900

    rename word resource

--- backend/demo/src/main/java/com/_2cha/demo/member/config/NicknameGeneratorConfig.java
+++ backend/demo/src/main/java/com/_2cha/demo/member/config/NicknameGeneratorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NicknameGeneratorConfig {
 
-  public static final String PREFIX_PATH = "static/nickname-prefix";
-  public static final String MIDDLE_PATH = "static/nickname-middle";
-  public static final String SUFFIX_PATH = "static/nickname-suffix";
+  public static final String PREFIX_PATH = "static/nickname-prefix.txt";
+  public static final String MIDDLE_PATH = "static/nickname-middle.txt";
+  public static final String SUFFIX_PATH = "static/nickname-suffix.txt";
 }
