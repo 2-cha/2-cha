3560b0340aec673229db14f6e089f8c4b0f9ca9c
chichoon <chichoon.choi@gmail.com>
2023-06-25 19:39:56 +0900

    feat: share button 공유 및 복사 완료시 문구 표시

--- frontend/ui/src/components/Buttons/ShareButton.tsx
+++ frontend/ui/src/components/Buttons/ShareButton.tsx
@@ -1,15 +1,82 @@
+import { useEffect, useState } from 'react';
+import cn from 'classnames';
+
 import { ShareIcon } from '../Icons';
 
 import s from './ShareButton.module.scss';
 
 interface Props {
+  sharedTitle: string;
+  sharedUrl: string;
+  sharedText?: string;
+  textAlign: 'left' | 'right';
   size?: number;
 }
 
-export default function ShareButton({ size = 24 }: Props) {
+const DEFAULT = 0;
+const IS_SHARED = 1;
+const IS_COPIED = 2;
+
+export default function ShareButton({
+  sharedTitle,
+  sharedUrl,
+  sharedText,
+  textAlign = 'left',
+  size = 24,
+}: Props) {
+  const [shareStatus, setShareStatus] = useState(DEFAULT);
+
+  useEffect(() => {
+    if (shareStatus === IS_SHARED || shareStatus === IS_COPIED) {
+      setTimeout(() => {
+        setShareStatus(DEFAULT);
+      }, 1000);
+    }
+  }, [shareStatus]);
+
+  function handleClickShare() {
+    if (shareStatus === IS_SHARED || shareStatus === IS_COPIED) return;
+    if (navigator.share) {
+      navigator
+        .share({
+          title: sharedTitle,
+          text: sharedText,
+          url: sharedUrl,
+        })
+        .then(() => {
+          setShareStatus(IS_SHARED);
+        })
+        .catch(console.error);
+    } else {
+      navigator.clipboard.writeText(sharedUrl).then(() => {
+        setShareStatus(IS_COPIED);
+      });
+    }
+  }
+
   return (
-    <button type="button" className={s.share}>
+    <button type="button" className={s.share} onClick={handleClickShare}>
+      {textAlign === 'left' && (
+        <span
+          className={cn(s.share__text, s.share__left, {
+            [s.hidden]: shareStatus === DEFAULT,
+          })}
+        >
+          {shareStatus === IS_SHARED && '공유 완료'}
+          {shareStatus === IS_COPIED && '복사 완료'}
+        </span>
+      )}
       <ShareIcon width={size} height={size} />
+      {textAlign === 'right' && (
+        <span
+          className={cn(s.share__text, s.share__right, {
+            [s.hidden]: shareStatus === DEFAULT,
+          })}
+        >
+          {shareStatus === IS_SHARED && '공유 완료'}
+          {shareStatus === IS_COPIED && '복사 완료'}
+        </span>
+      )}
     </button>
   );
 }
