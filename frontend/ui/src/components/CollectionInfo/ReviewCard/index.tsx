adf7336b79c0f276b4a9b5c8a8d796841d9d68c6
chichoon <chichoon.choi@gmail.com>
2023-06-25 14:33:52 +0900

    style: review card

--- frontend/ui/src/components/CollectionInfo/ReviewCard/index.tsx
+++ frontend/ui/src/components/CollectionInfo/ReviewCard/index.tsx
@@ -1,28 +1,41 @@
+import Image from 'next/image';
+
 import { Review } from '@/types';
+import { Tag } from '@/components/Tags';
 
 import s from './ReviewCard.module.scss';
-import Image from 'next/image';
+import NoImage from '@/components/NoImage';
 
 interface Props {
   review: Omit<Review, 'member'>;
 }
 
 export default function ReviewCard({ review }: Props) {
+  const { place } = review;
+
   return (
     <div className={s.wrapper}>
-      <div>컬렉션 카드</div>
-      <Image
-        src="https://picsum.photos/536/353"
-        alt="collection image"
-        width={480}
-        height={480}
-        className={s.wrapper__image}
-      />
-      {/* {review.images && review.images[0] ? (
-        <Image src={review.images[0]} width={480} height={480} />
+      {review.images && review.images[0] ? (
+        <Image
+          src={review.images[0]}
+          alt="collection image"
+          width={480}
+          height={480}
+          className={s.wrapper__image}
+        />
       ) : (
-        <div>이미지 없음</div>
-      )} */}
+        <NoImage className={s.noimage} />
+      )}
+      <div className={s.metadata}>
+        <span className={s.metadata__name}>{place.name}</span>
+        <span className={s.metadata__address}>{place.address}</span>
+        <div className={s.metadata__tags}>
+          {review.tags &&
+            review.tags.map((tag) => (
+              <Tag key={tag.id} tag={tag} className={s.metadata__tag} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
