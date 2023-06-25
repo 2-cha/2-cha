632002281ff70d18cbc4df0733c5a197fef1e47f
chichoon <chichoon.choi@gmail.com>
2023-06-25 20:37:34 +0900

    fix: isLoading, isError 일때 컴포넌트 렌더링 중지

--- frontend/ui/src/pages/collections/index.tsx
+++ frontend/ui/src/pages/collections/index.tsx
@@ -7,11 +7,15 @@ export default function Collections() {
   const { data, isLoading, isError } = useCollectionsQuery();
   return (
     <div>
-      {data.map((collection: Collection) => (
-        <Link href={`/collections/${collection.id}`} key={collection.id}>
-          {collection.title}
-        </Link>
-      ))}
+      {isLoading || isError ? null : (
+        <>
+          {data.map((collection: Collection) => (
+            <Link href={`/collections/${collection.id}`} key={collection.id}>
+              {collection.title}
+            </Link>
+          ))}
+        </>
+      )}
     </div>
   );
 }
