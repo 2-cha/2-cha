1e2edcba8422e7097d0815393d5714cf7e2b2e8d
chichoon <chichoon.choi@gmail.com>
2023-06-27 13:29:04 +0900

    fix: useCollection 인자 변경

--- frontend/ui/src/hooks/query/useCollections.ts
+++ frontend/ui/src/hooks/query/useCollections.ts
@@ -1,18 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchClient } from '@/lib/fetchClient';
+import { useCurrentLocation } from '../useCurrentLocation';
+import { Coordinate } from '@/atoms';
 
-async function fetchCollections() {
-  const { data } = await fetchClient.get<any>('/collections');
+interface Params {
+  location: Coordinate | null;
+  distance?: string;
+}
+
+async function fetchCollections({ location, distance }: Params) {
+  const params = { lat: location?.lat, lon: location?.lon, distance };
+  const { data } = await fetchClient.get<any>('/collections', { params });
 
   return data;
 }
 
-export function useCollectionsQuery() {
+export function useCollectionsQuery(distance?: string) {
+  const { location } = useCurrentLocation();
   const result = useQuery({
     queryKey: ['collections'],
-    queryFn: () => fetchCollections(),
+    queryFn: () => fetchCollections({ location, distance }),
     retry: false,
+    enabled: !!location,
   });
 
   return result;
