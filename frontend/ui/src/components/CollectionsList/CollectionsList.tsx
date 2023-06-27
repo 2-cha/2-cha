75004e53f51fdc53b10ecbbb2e2dc3a2af03c3d8
Ji Yoon Choi <chichoon.choi@gmail.com>
2023-06-27 15:19:29 +0900

    Feat: Collection List (#105)
    * fix: useCollection 인자 변경
    
    * feat: 컬렉션 목록 헤더 추가
    
    * feat: 컬렉션 컴포넌트
    
    * style: border 제거
    
    * style: collection 스타일 작성
    
    * fix: review card size
    
    * style: 컬렉션 제목 아래 margin

---
+++ frontend/ui/src/components/CollectionsList/CollectionsList.tsx
@@ -0,0 +1,26 @@
+import { Collection } from '@/types/collection';
+import CollectionsElement from './CollectionsElement';
+
+import s from './CollectionsList.module.scss';
+
+interface ListProps {
+  collections: Collection[];
+}
+
+export default function CollectionsList({ collections }: ListProps) {
+  // const { user } = useAuth();
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.wrapper__header}>
+        {/* <h1>{user?.name}님을 위한 컬렉션.</h1> */}
+        <h1>유저님을 위한 컬렉션을 준비했어요.</h1>
+      </div>
+      <ul className={s.wrapper__inner}>
+        {collections.map((collection) => (
+          <CollectionsElement key={collection.id} collection={collection} />
+        ))}
+      </ul>
+    </div>
+  );
+}
