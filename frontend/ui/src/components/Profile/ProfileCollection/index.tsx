c816fc6df12d525fcce08cd75bc9dd551bbab725
chichoon <chichoon.choi@gmail.com>
2023-06-28 15:42:12 +0900

    feat: :sparkles: 컬렉션 영역 내용물 추가

--- frontend/ui/src/components/Profile/ProfileCollection/index.tsx
+++ frontend/ui/src/components/Profile/ProfileCollection/index.tsx
@@ -1,7 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Link from 'next/link';
+import cn from 'classnames';
+
+import { PlusSquareIcon } from '@/components/Icons';
+
 import s from './ProfileCollection.module.scss';
+import 'swiper/css';
 
 export default function ProfileCollection() {
-  return <div className={s.root}>컬렉션</div>;
+  const collection = [
+    '컬렉션1',
+    '컬렉션2',
+    '컬렉션3',
+    '컬렉션4',
+    '컬렉션5',
+    '컬렉션6',
+  ];
+  const width = window.innerWidth;
+
+  return (
+    <div className={s.root}>
+      <Swiper
+        className={s.wrapper}
+        spaceBetween={10}
+        slidesPerView={width < 480 ? 4 : 5}
+        scrollbar
+      >
+        <SwiperSlide>
+          <Link
+            href="/write/collection"
+            className={cn(s.wrapper__add, s.element)}
+          >
+            <PlusSquareIcon withoutBorder />
+          </Link>
+        </SwiperSlide>
+        {collection.map((item) => (
+          <SwiperSlide key={item} className={s.wrapper__element}>
+            <Link href={`/collection/${item}`} className={s.element}>
+              {/* TODO: collection link */}
+              {item}
+              <img src={item} alt={`${item} collection thumbnail`} />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 // TODO: collection 완성
