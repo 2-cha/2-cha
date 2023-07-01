604a19b0e9e35194cf1671fa0a9ee532d79c8b48
chichoon <chichoon.choi@gmail.com>
2023-07-01 14:39:59 +0900

    style: 컬렉션 추가 탭 수정

--- frontend/ui/src/components/AddCollectionForm/index.tsx
+++ frontend/ui/src/components/AddCollectionForm/index.tsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { useAuth } from '@/hooks';
 import AddReviews from './AddReviews';
+import DecorateCover from './DecorateCover';
+import { ArrowIcon } from '../Icons';
+
+import s from './AddCollectionForm.module.scss';
 
 export interface CollectionFormData {
   title: string;
@@ -17,7 +21,7 @@ export default function AddCollectionForm() {
   const [selectedReview, setSelectedReview] = useState<number[]>([]);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const method = useForm<CollectionFormData>();
   const {
@@ -25,13 +29,29 @@ export default function AddCollectionForm() {
     formState: { errors },
   } = method;
 
+  const handleClickModifyCover = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+
   return (
     <FormProvider {...method}>
+      <button
+        type="button"
+        onClick={handleClickModifyCover}
+        className={s.openCover}
+      >
+        <ArrowIcon />
+        <span>표지 편집으로 돌아가기</span>
+      </button>
       <AddReviews
         selectedReviews={selectedReview}
         setSelectedReviews={setSelectedReview}
         memberId={memberId}
       />
+      <button type="submit" form="write" className={s.submit}>
+        <span>작성</span>
+      </button>
+      <DecorateCover isOpen={isOpen} setIsOpen={setIsOpen} />
     </FormProvider>
   );
 }
