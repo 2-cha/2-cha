3a1b7371861d3be32c92e6bc68420b56b8c78c7c
m1n-q <m1nq.dev@gmail.com>
2023-07-02 16:16:09 +0900

    limit size of tags in review

--- backend/demo/src/main/java/com/_2cha/demo/review/dto/WriteReviewRequest.java
+++ backend/demo/src/main/java/com/_2cha/demo/review/dto/WriteReviewRequest.java
@@ -1,6 +1,7 @@
 package com._2cha.demo.review.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 public class WriteReviewRequest {
 
   @NotNull
+  @Size(min = 1, max = 5)
   List<Long> tagIds;
   @NotNull
   List<String> imgUrls;
