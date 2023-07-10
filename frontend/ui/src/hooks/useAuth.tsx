6dd6396414046cdf214b5d0f8fd1a664d6d2d9a0
chichoon <chichoon.choi@gmail.com>
2023-07-10 16:35:26 +0900

    fix: :bug: requireAuth만 useEffect로 로그인 여부 체크하기

--- frontend/ui/src/hooks/useAuth.tsx
+++ frontend/ui/src/hooks/useAuth.tsx
@@ -6,20 +6,20 @@ import { jwtPayloadState } from '@/atoms';
 
 export function useAuth() {
   const jwtPayload = useRecoilValue(jwtPayloadState);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!jwtPayload) {
-      router.push('/login');
-    }
-  }, [jwtPayload, router]);
 
   return { user: jwtPayload };
 }
 
 export function requireAuth<P extends {}>(Component: React.ComponentType<P>) {
   return function AuthenticateWrapper(props: P) {
-    useAuth();
+    const jwtPayload = useRecoilValue(jwtPayloadState);
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!jwtPayload) {
+        router.push('/login');
+      }
+    }, [jwtPayload, router]);
 
     return <Component {...props} />;
   };
