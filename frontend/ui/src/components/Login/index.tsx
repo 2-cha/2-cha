339b49c07d093cbeb2585019854ec30d944aabde
chichoon <chichoon.choi@gmail.com>
2023-07-10 21:26:45 +0900

    style: :lipstick: 구글 로그인 이미지 붙임

--- frontend/ui/src/components/Login/index.tsx
+++ frontend/ui/src/components/Login/index.tsx
@@ -1,4 +1,5 @@
 import { GOOGLE_AUTH_URL } from '@/lib/auth';
+import Image from 'next/image';
 
 import s from './Login.module.scss';
 
@@ -7,7 +8,12 @@ export default function Login() {
     <div className={s.root}>
       <h1>Sign in</h1>
       <a href={GOOGLE_AUTH_URL} className={s.oauth}>
-        Continue with Google
+        <Image
+          src="/google_signin.png"
+          width={400}
+          height={100}
+          alt="google logo"
+        />
       </a>
     </div>
   );
