f41d5b5cd4f27a31b01ad98606ce5a0e80188ddc
seushin <seushin@student.42seoul.kr>
2023-07-13 20:57:46 +0900

    add manifest, icons

--- frontend/ui/src/components/MetaData.tsx
+++ frontend/ui/src/components/MetaData.tsx
@@ -16,6 +16,44 @@ export default function MetaData({
       <meta name="description" content={description} />
 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/images/icons-72.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/icons-120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/icons-144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/icons-180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/images/icons-192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/icons-96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/icons-16.png"
+      />
+
+      <meta name="theme-color" content="#ed8672" />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://2cha.place" />
