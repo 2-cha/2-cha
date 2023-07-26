22787291a3145ded30a73b9ae81bc6713563c128
m1n-q <m1nq.dev@gmail.com>
2023-07-26 21:43:27 +0900

    refactor: apply condition on `AuthFilter` at management context
    Enabling `AuthFilter` can be done with `management.authorization.enabled`

--- backend/demo/src/main/java/com/_2cha/actuator/config/ManagementContextFilterConfig.java
+++ backend/demo/src/main/java/com/_2cha/actuator/config/ManagementContextFilterConfig.java
@@ -9,8 +9,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
+import org.springframework.boot.actuate.autoconfigure.web.server.ConditionalOnManagementPort;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 
 @ManagementContextConfiguration
 public class ManagementContextFilterConfig {
@@ -21,7 +26,21 @@ public class ManagementContextFilterConfig {
   @Autowired
   ObjectMapper objectMapper;
 
+  /**
+   * NOTE: Authorization is unnecessary for cluster-internal usage.
+   * <p>
+   * <p>
+   * Filter should be enabled when:
+   * <p>
+   * 1. If using same port with API server
+   * <p>
+   * 2. If using different port but it is exposed to external
+   * <p>
+   * In case of 1, it is enabled by default. But in case of 2, need to enable it manually by
+   */
+
   @Bean
+  @Conditional(OnSamePortOrProperty.class)
   public FilterRegistrationBean<AuthFilter> enableAuthFilter() {
     FilterRegistrationBean<AuthFilter> filterRegistrationBean = new FilterRegistrationBean<>();
     filterRegistrationBean.setFilter(new AuthFilter(authService));
@@ -38,5 +57,20 @@ public class ManagementContextFilterConfig {
     filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
     filterRegistrationBean.setOrder(HIGHEST_PRECEDENCE);
     return filterRegistrationBean;
+  }
+
+  static class OnSamePortOrProperty extends AnyNestedCondition {
+
+    OnSamePortOrProperty() {
+      super(ConfigurationPhase.REGISTER_BEAN);
+    }
+
+    @ConditionalOnManagementPort(ManagementPortType.SAME)
+    static class OnSamePort {
+    }
+
+    @ConditionalOnProperty(name = "management.authorization.enabled", havingValue = "true")
+    static class OnProperty {
+    }
   }
 }
