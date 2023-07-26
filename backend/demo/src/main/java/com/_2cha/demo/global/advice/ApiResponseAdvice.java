ac732a004885c34ac9f62d234b4e7ab3cb46cfc0
m1n-q <m1nq.dev@gmail.com>
2023-07-27 01:16:29 +0900

    wrap as `ApiResponse` only if JacksonConverter

--- backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponseAdvice.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponseAdvice.java
@@ -3,7 +3,6 @@ package com._2cha.demo.global.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
@@ -20,13 +19,12 @@ public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
   public boolean supports(MethodParameter returnType,
                           Class<? extends HttpMessageConverter<?>> converterType) {
 
-    return ResponseEntity.class.isAssignableFrom(returnType.getParameterType()) &&
-           MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
 
     /**
-     * return true 였을때, String 타입 들어온 경우 에러 사항 기록하기.
      * class com._2cha.demo.global.advice.ApiResponse cannot be cast to class java.lang.String (com._2cha.demo.global.advice.ApiResponse is in unnamed module of loader org.springframework.boot.devtools.restart.classloader.RestartClassLoader @2ce05c85; java.lang.String is in module java.base of loader 'bootstrap')
-     * 이때는 아마 StringHttpMessageConverter 가 돌아가도록 되어 있는데, 중간에 Body를 바꿔치기해서 고장난듯.
+     * StringHttpMessageConverter 가 기대하는 String Body를 객체(ApiResponse)로 바꿔서 생긴 현상.
+     * 객체를 처리할 수 있는 MappingJackson2HttpMessageConverter 인 경우에만 beforeBodyWrite()를 호출하도록 설정.
      */
   }
 
