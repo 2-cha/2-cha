cccecfcf5fec243d599e5cf3e41589fd787dbbf4
m1n-q <68891716+m1n-q@users.noreply.github.com>
2023-07-27 01:19:53 +0900

    fix: api response (#140)
    * wrap as `ApiResponse` only if JacksonConverter
    
    * set status enum on `ApiResponse` correctly
    
    * (rollback) remove default value of status and code

--- backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponse.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponse.java
@@ -1,17 +1,17 @@
 package com._2cha.demo.global.advice;
 
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 @Data
 public class ApiResponse<T> {
 
   private boolean success = true;
-  private HttpStatusCode status = HttpStatus.OK;
+  private HttpStatusCode status;
   private T data;
 
-  public ApiResponse(T data) {
+  public ApiResponse(HttpStatusCode status, T data) {
+    this.status = status;
     this.data = data;
   }
 }
--- backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponseAdvice.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/advice/ApiResponseAdvice.java
@@ -2,12 +2,13 @@ package com._2cha.demo.global.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -20,13 +21,12 @@ public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
   public boolean supports(MethodParameter returnType,
                           Class<? extends HttpMessageConverter<?>> converterType) {
 
-    return ResponseEntity.class.isAssignableFrom(returnType.getParameterType()) &&
-           MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
 
     /**
-     * return true 였을때, String 타입 들어온 경우 에러 사항 기록하기.
      * class com._2cha.demo.global.advice.ApiResponse cannot be cast to class java.lang.String (com._2cha.demo.global.advice.ApiResponse is in unnamed module of loader org.springframework.boot.devtools.restart.classloader.RestartClassLoader @2ce05c85; java.lang.String is in module java.base of loader 'bootstrap')
-     * 이때는 아마 StringHttpMessageConverter 가 돌아가도록 되어 있는데, 중간에 Body를 바꿔치기해서 고장난듯.
+     * StringHttpMessageConverter 가 기대하는 String Body를 객체(ApiResponse)로 바꿔서 생긴 현상.
+     * 객체를 처리할 수 있는 MappingJackson2HttpMessageConverter 인 경우에만 beforeBodyWrite()를 호출하도록 설정.
      */
   }
 
@@ -36,10 +36,13 @@ public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
                                 Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                 ServerHttpRequest request, ServerHttpResponse response) {
 
-    // ExceptionHandler has higher priority
+    // ExceptionHandler has higher priority,
+    // so if exception was thrown `ResponseEntity#body` was already wrapped as ApiError
     if (body instanceof ApiError<?>) {
       return body;
     }
-    return new ApiResponse<>(body); //TODO: set status
+    ServletServerHttpResponse servletResponse = (ServletServerHttpResponse) response;
+    int status = servletResponse.getServletResponse().getStatus();
+    return new ApiResponse<>(HttpStatusCode.valueOf(status), body);
   }
 }
