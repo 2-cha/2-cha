96443c46a2f85f9a878eac6dea419fe51a03294b
m1n-q <m1nq.dev@gmail.com>
2023-08-12 17:50:15 +0900

    refactor: increase thread pool size

--- backend/demo/src/main/java/com/_2cha/demo/global/config/AsyncConfig.java
+++ backend/demo/src/main/java/com/_2cha/demo/global/config/AsyncConfig.java
@@ -25,9 +25,9 @@ public class AsyncConfig {
   @Bean
   Executor imageUploadTaskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(3);
-    executor.setMaxPoolSize(3);
-    executor.setQueueCapacity(10);
+    executor.setCorePoolSize(10);
+    executor.setMaxPoolSize(20);
+    executor.setQueueCapacity(50);
     executor.setThreadNamePrefix("img-uploader");
 
     return executor;
